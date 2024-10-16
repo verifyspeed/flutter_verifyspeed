26a57aa351c0c959df0dbb05b3cdfb6daf6efada
rekananwar <rekan545@gmail.com>
2024-10-16 07:35:41 +0300

    Export VerifySpeedErrorType

--- lib/flutter_verifyspeed.dart
+++ lib/flutter_verifyspeed.dart
@@ -2,4 +2,4 @@ library flutter_verifyspeed;
 
 export 'src/src.dart';
 export 'package:flutter_verifyspeed_plugin/flutter_verifyspeed_plugin.dart'
-    show VerifySpeedError;
+    show VerifySpeedError, VerifySpeedErrorType;
